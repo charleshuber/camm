040547e17af269d4c573798d95cefe4107f672a6
charleshuber <charles.huber@hotmail.fr>
2019-04-05 06:42:34 +0100

    config for raspberry

--- src/main/java/com/surfthevoid/camm/Application.java
+++ src/main/java/com/surfthevoid/camm/Application.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) throws Exception {
     	System.out.println(System.getProperty("java.library.path"));
+	System.out.println("Start loading libs");
     	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	System.out.println("End loading libs");
     	SpringApplication.run(Application.class, args);
     }
 }
